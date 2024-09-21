35c0c58bf8d1116a05f26a1b8cdd5ab5b6a575a7
naimur-234964 <nrsazib234964@gmail.com>
2024-09-21 23:49:37 +0600

    jS started

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
